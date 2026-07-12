ea2af42cf3d823f5f494e5bacb30b73af97e8d5f
03kousei29 <138800389+03kousei29@users.noreply.github.com>
2026-07-12 21:09:23 +0900

    ローカル版もどし

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // Firebase Console > プロジェクトの設定 > マイアプリ > SDK の設定と構成
 // に表示される firebaseConfig の値へ置き換えてください。
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCkgnIGyuvWAKBbVxflEfJNPwOFA3km4LQ",
+  authDomain: "anime-score-lab.firebaseapp.com",
+  projectId: "anime-score-lab",
+  storageBucket: "anime-score-lab.firebasestorage.app",
+  messagingSenderId: "914239113396",
+  appId: "1:914239113396:web:e27640670a0c79a3fcefe1",
+  measurementId: "G-9MX3LQV33E"
 };
